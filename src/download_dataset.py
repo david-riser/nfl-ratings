b8d9d0808e3130e38d0bee40ebffe8fe7545333e
David Riser <dmriser@gmail.com>
2019-09-22 11:02:53 -0400

    Initial Setup
    I have added a few files and the basic directory structure.

---
+++ src/download_dataset.py
@@ -0,0 +1,28 @@
+#
+#    David Riser
+#    Sept. 21, 2019
+#    download_dataset.py
+#
+#    The blog FiveThirtyEight has made historical as well as recent
+#    data from NFL games available.  In this script, the latest files
+#    are downloaded to the data/ directory in the top folder of this
+#    project.
+#
+
+import os
+from urllib import request
+
+if __name__ == '__main__':
+
+    data_dir = os.path.abspath('../data')
+    print(f'Downloading NFL datasets into {data_dir}...')
+
+    # Data provided by FiveThirtyEight, https://fivethirtyeight.com/
+    history_url = 'https://projects.fivethirtyeight.com/nfl-api/nfl_elo.csv'
+    latest_url = 'https://projects.fivethirtyeight.com/nfl-api/nfl_elo_latest.csv'
+
+    # Download each file, if the directory structure is changed, these lines will
+    # need to change as well.
+    request.urlretrieve(history_url, f'{data_dir}/historical_data.csv')
+    request.urlretrieve(latest_url, f'{data_dir}/latest_data.csv')
+    print('Done downloading data!')
